9843ff12830684f151c10d7ab27110b8e9fbd0df
mga-liborio <mga.liborio@gmail.com>
2021-12-17 13:59:13 -0300

    deploy modulo node

--- projetoFinal/backend/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# from .models import Category, Vehicle
-
-# Register your models here.
-
-# admin.site.unregister(Category)
-# admin.site.unregister(Vehicle)
--- projetoFinal/backend/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
-
-
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    model = User
